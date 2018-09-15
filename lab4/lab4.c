b25b16861990012e1a889769af538d756d34a860
Rodrigo Villardi Cortezi <rodrigovcortezi@gmail.com>
2018-09-15 19:43:09 -0300

    Termina o labortório 4.

--- lab4/lab4.c
+++ lab4/lab4.c
@@ -25,7 +25,7 @@ void exercicio1() {
     int pid, status;
 
     if(pipe(fd) < 0) {
-	printf("Error when opening pipes.\n");
+	printf("error when opening pipes.\n");
 	exit(-1);
     }
 
@@ -35,14 +35,13 @@ void exercicio1() {
 	close(fd[1]);
 	wait(&status);
 	read(fd[0], buffer, sizeof(buffer));
-	printf("Processo pai leu: %s\n", buffer);
+	printf("%s\n", buffer);
 	close(fd[0]);
     } else if(pid == 0) {
 	// Child process
 	close(fd[0]);
 	strcpy(buffer, "Hello world!\n");
 	write(fd[1], buffer, sizeof(buffer));
-	printf("Processo filho escreveu: %s\n");
 	close(fd[1]);
 	exit(0);
     } else {
@@ -100,45 +99,46 @@ void exercicio2() {
 //
 
 void exercicio3() {
-    char *argv[1] = {"ls"};
-    char *argv2[2] = {"echo"};
-    const int stdout_fd = 1;
+    int i;
+    int pid;
     int fd[2];
+    char *argv[] = {"ls", NULL};
+    char *argv2[3];
     char buffer;
-    int i, k;
-    char *filename, filenames[500];
-    int bytes_read;
-    int pid, status;
+    char filenames[100];
+    int size;
+
+
     if(pipe(fd) < 0) {
-	printf("Error when opening pipes.\n");
+	printf("error when opening pipes.\n");
 	exit(-1);
     }
 
     for(i = 0; i < 2; i++) {
 	pid = fork();
 	if(pid == 0) {
-	    // Child process
 	    if(i == 0) {
 		close(fd[0]);
-		dup2(fd[1], stdout_fd);
-		execve("/usr/bin/ls", argv, NULL);
+		dup2(fd[1], 1);
+		execve("/bin/ls", argv, NULL);
 	    } else {
 		close(fd[1]);
-		k = 0;
-		do {
-		    bytes_read = read(fd[0], &filenames[k], 1);
-		    k++;
-		} while(bytes_read > 0);
+		size = 0;
+		while(read(fd[0], &buffer, 1) == 1) {
+		    filenames[size] = buffer;
+		    size++;
+		}
+		filenames[size] = '\0';
+		argv2[0] = "echo";
 		argv2[1] = filenames;
-		printf("%s\n", filenames);
-		execve("/usr/bin/echo", argv2, NULL);
+		argv2[2] = NULL;
+		execve("/bin/echo", argv2, NULL);
 	    }
-	    exit(0);
-	} else if(pid < 0){
-	    // Fork error
-	    printf("Error when forking.\n");
+	} else if(pid < 0) {
+	    printf("Fork error.\n");
 	    exit(-1);
 	}
     }
+
 }
 
