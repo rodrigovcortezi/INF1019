87d7d40075c47e21537b8aba0e285e0b05ff9299
RODRIGO VILLARDI CORTEZI <c1511425@mundau.grad.inf.puc-rio.br>
2018-09-20 16:18:49 -0300

    Exercicios 1 e 2 do laboratorio 5.

---
+++ lab5/lab5.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <pthread.h>
+
+
+static void *contCrescente();
+static void *contDecrescente();
+static void *incrementa1();
+static void *incrementa5();
+void exercicio1();
+void exercicio2();
+void exercicio3();
+
+int global = 0;
+
+int main()
+{
+    // exercicio1();
+    exercicio2();
+
+    return 0;
+}
+
+void exercicio1() {
+    pthread_t thread1;
+    pthread_t thread2;
+
+    pthread_create(&thread1, NULL, contCrescente, NULL);
+    pthread_create(&thread2, NULL, contDecrescente, NULL);
+    pthread_join(thread1, NULL);
+    pthread_join(thread2, NULL);
+}
+
+void exercicio2() {
+    pthread_t thread1;
+    pthread_t thread2;
+
+    pthread_create(&thread1, NULL, incrementa1, NULL);
+    pthread_create(&thread2, NULL, incrementa5, NULL);
+    pthread_join(thread1, NULL);
+    pthread_join(thread2, NULL);
+}
+
+void exercicio3() {
+
+}
+
+static void *contCrescente() {
+    int i;
+    for(i = 1; i <= 20; i++) {
+	printf("%d \n", i);
+	sleep(2);
+    }
+    pthread_exit(NULL);
+}
+
+static void *contDecrescente() {
+    int i;
+    for(i = 30; i >= 1; i--) {
+	printf("%d \n", i);
+	sleep(1);
+    }
+    pthread_exit(NULL);
+}
+
+static void *incrementa1() {
+    global += 1;
+    printf("thread1, global: %d\n", global);
+    pthread_exit(NULL);
+}
+
+static void *incrementa5() {
+    global += 5;
+    printf("thread2, global: %d\n", global);
+    pthread_exit(NULL);
+}
